752a7e966a082aee52e229b2d4ab5bc71ea0ee84
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-05-02 18:39:24 +0530

    Refatore exitItems

--- Layered_Architecture/src/controller/ManageItemsFormController.java
+++ Layered_Architecture/src/controller/ManageItemsFormController.java
@@ -223,10 +223,8 @@ public class ManageItemsFormController {
 
 
     private boolean existItem(String code) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("SELECT code FROM Item WHERE code=?");
-        pstm.setString(1, code);
-        return pstm.executeQuery().next();
+      ItemDAOImpl itemDAO = new ItemDAOImpl();
+      return itemDAO.exitItems(code);
     }
 
 
