fc0c1717967de51ef6f640d48b6018a70c95091c
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-05-02 14:09:06 +0530

    add savecustomer method

--- Layered_Architecture/src/dao/CustomerDAOImpl.java
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -3,10 +3,7 @@ package dao;
 import db.DBConnection;
 import model.CustomerDTO;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CustomerDAOImpl {
@@ -26,5 +23,12 @@ public class CustomerDAOImpl {
         return allCustomers;
     }
 
-
+    public boolean saveCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1, dto.getId());
+        pstm.setString(2, dto.getName());
+        pstm.setString(3, dto.getAddress());
+        return pstm.executeUpdate()>0;
+    }
 }
