015c7ba9b40b25990be357e5a17983ce0df136e7
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-05-02 20:44:33 +0530

    Refacter saveOrder

--- Layered_Architecture/src/controller/PlaceOrderFormController.java
+++ Layered_Architecture/src/controller/PlaceOrderFormController.java
@@ -317,62 +317,9 @@ public class PlaceOrderFormController {
 
     public boolean saveOrder(String orderId, LocalDate orderDate, String customerId, List<OrderDetailDTO> orderDetails) {
         /*Transaction*/
-        Connection connection = null;
+
         try {
-            connection = DBConnection.getDbConnection().getConnection();
-            PreparedStatement stm = connection.prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
-            stm.setString(1, orderId);
-            /*if order id already exist*/
-            if (stm.executeQuery().next()) {
-
-            }
-
-            connection.setAutoCommit(false);
-            stm = connection.prepareStatement("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
-            stm.setString(1, orderId);
-            stm.setDate(2, Date.valueOf(orderDate));
-            stm.setString(3, customerId);
-
-            if (stm.executeUpdate() != 1) {
-                connection.rollback();
-                connection.setAutoCommit(true);
-                return false;
-            }
-
-            stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
-
-            for (OrderDetailDTO detail : orderDetails) {
-                stm.setString(1, orderId);
-                stm.setString(2, detail.getItemCode());
-                stm.setBigDecimal(3, detail.getUnitPrice());
-                stm.setInt(4, detail.getQty());
-
-                if (stm.executeUpdate() != 1) {
-                    connection.rollback();
-                    connection.setAutoCommit(true);
-                    return false;
-                }
-
-//                //Search & Update Item
-                ItemDTO item = findItem(detail.getItemCode());
-                item.setQtyOnHand(item.getQtyOnHand() - detail.getQty());
-
-                PreparedStatement pstm = connection.prepareStatement("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?");
-                pstm.setString(1, item.getDescription());
-                pstm.setBigDecimal(2, item.getUnitPrice());
-                pstm.setInt(3, item.getQtyOnHand());
-                pstm.setString(4, item.getCode());
-
-                if (!(pstm.executeUpdate() > 0)) {
-                    connection.rollback();
-                    connection.setAutoCommit(true);
-                    return false;
-                }
-            }
-
-            connection.commit();
-            connection.setAutoCommit(true);
-            return true;
+            return new PlaceOrderDAOImpl().saveOrder(orderId,orderDate,customerId,orderDetails);
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
--- Layered_Architecture/src/dao/PlaceOrderDAOImpl.java
+++ Layered_Architecture/src/dao/PlaceOrderDAOImpl.java
@@ -3,9 +3,12 @@ package dao;
 import db.DBConnection;
 import model.ItemDTO;
 import model.OrderDTO;
+import model.OrderDetailDTO;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PlaceOrderDAOImpl {
 
@@ -55,6 +58,7 @@ public class PlaceOrderDAOImpl {
 
 
 
+
     public String getCustomerName(String id) throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         Statement stm = connection.createStatement();
@@ -70,6 +74,65 @@ public class PlaceOrderDAOImpl {
         pstm.setString(1, id);
         return pstm.executeQuery().next();
     }
+
+    public boolean saveOrder(String orderId, LocalDate orderDate, String customerId, List<OrderDetailDTO> orderDetails) throws SQLException, ClassNotFoundException {
+        Connection connection = null;
+        connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement stm = connection.prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
+        stm.setString(1, orderId);
+        /*if order id already exist*/
+        if (stm.executeQuery().next()) {
+
+        }
+
+        connection.setAutoCommit(false);
+        stm = connection.prepareStatement("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
+        stm.setString(1, orderId);
+        stm.setDate(2, Date.valueOf(orderDate));
+        stm.setString(3, customerId);
+
+        if (stm.executeUpdate() != 1) {
+            connection.rollback();
+            connection.setAutoCommit(true);
+            return false;
+        }
+
+        stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
+
+        for (OrderDetailDTO detail : orderDetails) {
+            stm.setString(1, orderId);
+            stm.setString(2, detail.getItemCode());
+            stm.setBigDecimal(3, detail.getUnitPrice());
+            stm.setInt(4, detail.getQty());
+
+            if (stm.executeUpdate() != 1) {
+                connection.rollback();
+                connection.setAutoCommit(true);
+                return false;
+            }
+
+//                //Search & Update Item
+            ItemDTO item = findItem(detail.getItemCode());
+            item.setQtyOnHand(item.getQtyOnHand() - detail.getQty());
+
+            PreparedStatement pstm = connection.prepareStatement("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?");
+            pstm.setString(1, item.getDescription());
+            pstm.setBigDecimal(2, item.getUnitPrice());
+            pstm.setInt(3, item.getQtyOnHand());
+            pstm.setString(4, item.getCode());
+
+            if (!(pstm.executeUpdate() > 0)) {
+                connection.rollback();
+                connection.setAutoCommit(true);
+                return false;
+            }
+        }
+
+        connection.commit();
+        connection.setAutoCommit(true);
+        return true;
+    }
+
 
 
 
